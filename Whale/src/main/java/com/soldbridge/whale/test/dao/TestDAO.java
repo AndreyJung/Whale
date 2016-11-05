ad89196cc5abd77ba528efe4f81f49776566e472
aegisx0828 <aegisx0828@gmail.com>
2016-11-05 16:16:37 +0900

    앱, 서버 연동 테스트 기능 추가

---
+++ Whale/src/main/java/com/soldbridge/whale/test/dao/TestDAO.java
@@ -0,0 +1,26 @@
+package com.soldbridge.whale.test.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.soldbridge.whale.common.common.CommandMap;
+import com.soldbridge.whale.common.dao.AbstractDAO;
+
+@Repository("testDAO")
+public class TestDAO extends AbstractDAO{
+	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectTestTotRepu(Map<String, Object> map) {
+		return (List<Map<String, Object>>)selectList("selectTestTotRepu",map);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Map<String, Object>> selectTestTotRepuParam(Map<String, Object> map) {
+		return (List<Map<String, Object>>)selectList("selectTestTotRepuParam",map);
+	}
+}
+
+
+
